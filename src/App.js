13fb1869eba78010bea2e30ede05bc5f9c370678
Paul <lazar_paul_2006@yahoo.com>
2023-05-22 11:29:40 +0100

    Added the first custom component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  // Adding a paragraph using JS
+  // const paragraph = document.createElement('p');
+  // paragraph.textContent = 'This is inserted using JS';
+  // document.getElementById('root').appendChild(paragraph);
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
