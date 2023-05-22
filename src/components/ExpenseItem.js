062bf5494663ad60f5ed5133739d0193f5b2955e
Paul <lazar_paul_2006@yahoo.com>
2023-05-22 12:11:48 +0100

    Made ExpenseItem partially dynamic.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 5, 28); // the date between () is the default date
+  const expenseTitle = "Car insurance";
+  const expenseAmount = 249.67;
   return (
   <div className="expense-item">
-    <div>March 28th 2023</div>
+    <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <div className="expense-item__price">£249.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">£{expenseAmount}</div>
       </div>
   </div>
   );
