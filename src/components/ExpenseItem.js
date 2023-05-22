2a870d0f5d7cd35994c8f281c413d16cb897954d
Paul <lazar_paul_2006@yahoo.com>
2023-05-22 12:01:20 +0100

    Added styling to ExpenseItem component.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-  <div>
+  <div className="expense-item">
     <div>March 28th 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car insurance</h2>
+        <div className="expense-item__price">£249.67</div>
       </div>
-    <div>£249.67</div>
   </div>
   );
 };
