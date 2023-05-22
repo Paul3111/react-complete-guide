55b6030b712195b5c68ef6c6dd485398a90a9cf8
Paul <lazar_paul_2006@yahoo.com>
2023-05-22 12:43:14 +0100

    Using props.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 5, 28); // the date between () is the default date
-  const expenseTitle = "Car insurance";
-  const expenseAmount = 249.67;
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2023, 5, 28); // the date between () is the default date
+  // const expenseTitle = "Car insurance";
+  // const expenseAmount = 249.67;
+
   return (
   <div className="expense-item">
-    <div>{expenseDate.toISOString()}</div>
+    <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">£{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.amount}</div>
       </div>
   </div>
   );
