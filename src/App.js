b1d1b6a2822e215b7a47bc6fe3f4681a7d6672c6
Paul <lazar_paul_2006@yahoo.com>
2023-05-23 23:05:27 +0100

    Added the new expense form and css.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/newExpense/NewExpense';
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
