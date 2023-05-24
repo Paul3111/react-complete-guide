f222399333cc7a76fea1d9ac819a6d4ff89ceaff
Paul <lazar_paul_2006@yahoo.com>
2023-05-24 12:52:13 +0100

    Listening to user input.

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -2,17 +2,21 @@ import React from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+
   return <form>
-    <div class="new-expense__controls">
-      <div class="new-expense__control">
+    <div className="new-expense__controls">
+      <div className="new-expense__control">
         <label>Title</label>
-        <input type="text"/>
+        <input type="text" onChange={titleChangeHandler}/>
       </div>
-      <div class="new-expense__control">
+      <div className="new-expense__control">
         <label>Amount</label>
         <input type="number" min="0.01" step="0.01"/>
       </div>
-      <div class="new-expense__control">
+      <div className="new-expense__control">
         <label>Date</label>
         <input type="date" min="2023-01-01" max="2026-12-31"/>
       </div>
