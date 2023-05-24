c4f09388d63416d307667128ed78ab35af8ab493
Paul <lazar_paul_2006@yahoo.com>
2023-05-24 16:47:46 +0100

    Using two-way binding when saving the new expense entries.

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -27,6 +27,9 @@ const ExpenseForm = () => {
       date: new Date(enteredDate)
     };
     console.log(expenseData)
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -34,15 +37,15 @@ const ExpenseForm = () => {
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text" onChange={titleChangeHandler}/>
+        <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
       </div>
       <div className="new-expense__control">
         <label>Amount</label>
-        <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+        <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
       </div>
       <div className="new-expense__control">
         <label>Date</label>
-        <input type="date" min="2023-01-01" max="2026-12-31" onChange={dateChangeHandler}/>
+        <input type="date" min="2023-01-01" max="2026-12-31" value={enteredDate} onChange={dateChangeHandler}/>
       </div>
     </div>
     <div className="new-expense__actions">
