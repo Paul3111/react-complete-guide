627c1f2d25a9351711bee8fa136d0ffe7ec2e03c
Paul <lazar_paul_2006@yahoo.com>
2023-05-25 19:24:49 +0100

    The list is now filtered by date.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,18 +11,22 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+ });
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        {props.items.map( (expense) => (
-        <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        />
-          ))}
+        {filteredExpenses.map( (expense) => (
+          <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
