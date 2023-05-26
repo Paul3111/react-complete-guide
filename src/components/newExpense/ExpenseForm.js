b6bea47700b9115a8c63a4ba88d83d88957b3f95
Paul <lazar_paul_2006@yahoo.com>
2023-05-26 17:27:19 +0100

    Added buttons to show/hide the new expense form.

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -33,7 +33,7 @@ const ExpenseForm = (props) => {
   };
 
   return (
-  <form onSubmit={submitHandler}>
+  <form id="form" onSubmit={submitHandler}>
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
@@ -49,6 +49,7 @@ const ExpenseForm = (props) => {
       </div>
     </div>
     <div className="new-expense__actions">
+      <button type="button" onClick={props.onCancel}>Cancel</button>
       <button type="submit">Add Expense</button>
     </div>
   </form>
